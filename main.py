9d8848e72120e8a9772408f7199804153d8f9499
Jonny McFadden <jmcfadden1528@gmail.com>
2020-06-22 17:59:36 -0500

    added html index page

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template('hello_form.html')
+    return template.render()
+
+app.run()
